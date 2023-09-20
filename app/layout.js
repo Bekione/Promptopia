47fe8552f293413229a77399a826ecd592f8017f
Bekione <kinber024@gmail.com>
2023-09-20 15:22:11 +0300

    Add metadata

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,19 @@ import "@styles/globals.css";
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
+  generator: "Next.js",
+  applicationName: "Promptopia",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Prompt", "Share", "Ai"],
+  authors: [{ name: "Bereket", url: "https://promptopia-bek.vercel.app/" }],
+  colorScheme: "light",
+  creator: "Bereket Kinfe",
+  publisher: "Vercel",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 const RootLayout = ({ children }) => {
